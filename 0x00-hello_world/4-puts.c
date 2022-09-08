6f0197880b1a3cb151b746c9a880d30785f1f31e
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-08 10:31:50 -0700

    My commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
+/**
+ *
+ * * main - Printing
+ *
+ * *
+ *
+ * * Return: Always 0 (Successful)
+ *
+ * */
 
-	   puts("Programming is like building a multilingual puzzle");
+int main(void)
 
-		    getchar();
+{
 
-		       return 0;
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
 
 }
