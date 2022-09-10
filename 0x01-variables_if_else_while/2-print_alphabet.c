038474b69f9efcf5b94bfd10f5d39e15c1f872f4
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-10 05:02:07 -0700

    My commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-/** 
+/**
  * main - prints the alphabet in lowercase
  *
  * Return: 0 on success
  */
-int main (void)
+int main(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
-	put char (c);
+	putchar (c);
 	c++;
 	}
 	putchar('\n');
