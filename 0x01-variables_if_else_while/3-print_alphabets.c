525956f44c0cfc711e1acd2ccdc0468c7c7d3506
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-10 07:21:54 -0700

    My commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - lowercase alphabets
+ * main -entry block
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char c;
+	char C;
 
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-
+	for (C = 'A'; C <= 'Z'; C++)
+		putchar(C);
 	putchar('\n');
 	return (0);
 }
