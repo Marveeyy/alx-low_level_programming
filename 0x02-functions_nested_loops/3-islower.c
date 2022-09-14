9b11df7b1ab2e03ff974637088cb91046a715783
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-13 19:55:16 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,9 @@
 /**
  * _islower - Return 1 if letter is lowercase, 0 if not
  * @c: char type letter
- * Return : 1 if lowercase, 0 of not lowercase
+ * Return: 1 if lowercase, 0 if not lowercase
  */
-int _islower(int c);
+int _islower(void)
 {
 	if (c > 96 && c < 123)
 		return (1);
