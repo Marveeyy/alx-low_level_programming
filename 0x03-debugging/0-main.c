9facdb4b0cad5e721161af6d2d38dc86f2c26f96
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-15 01:51:01 +0000

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,12 @@
  * maon - tests function that prints if integer is positive or negative
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or_negative(i)
+	positive_or_negative(i);
 
-		return (0);
+	return (0);
 }
