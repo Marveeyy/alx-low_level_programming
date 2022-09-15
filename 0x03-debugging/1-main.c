3a944a77d648b595f9af119798c394690d954560
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-15 03:45:56 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - causes an indefinite loop
- * Return: 0
+ * Return: 0 on success
  */
 
 int main(void)
 {
 	int i;
 
-	printf(*Indefinite loop incoming : (\n *);
+	printf(*Indefinite loop incoming : (\n ");
 			i = 0;
 /*
  * while (i < 10)
