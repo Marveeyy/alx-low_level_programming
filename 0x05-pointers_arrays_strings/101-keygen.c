e9ced26392c83017427eaad976f34b7484f6366d
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-19 15:28:16 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwords for the program
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (b < 2772)
+	{
+		a = rand() % 128;
+		if ((b + a) > 2772)
+			break;
+		b += a;
+		c++;
+		printf("%c", i);
+	}
+	printf("%c\n", (2772 - b));
+	return (0);
+}
