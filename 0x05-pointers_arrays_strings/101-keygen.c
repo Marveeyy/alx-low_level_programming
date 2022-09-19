43f94ff12aabb9f15096416faa60e8fe25416de3
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-19 15:50:22 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,22 +11,44 @@
 
 int main(void)
 {
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	time_t t;
+	srand(time(0));
 
-	srand((unsigned int) time(&t));
-	while (b < 2772)
+	while (sum < 2772)
 	{
-		a = rand() % 128;
-		if ((b + a) > 2772)
-			break;
-		b += a;
-		c++;
-		printf("%c", a);
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
-	printf("%c\n", (2772 - b));
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
+
 	return (0);
 }
+
