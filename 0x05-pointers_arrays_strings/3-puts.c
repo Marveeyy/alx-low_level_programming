940697c0e519694682d4885ccf0eac81585a3bd8
Marveeyy <jmarvellous.wes@gmail.com>
2022-09-19 11:15:24 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - puts a string followed by a new line
+ *
+ * @str: string
+ *
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '0\')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
